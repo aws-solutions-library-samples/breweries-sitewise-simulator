9035ad8153cccf4c86e4b93f47c4073b9a832a69
Nicholas Santucci <nictooch@amazon.com>
2022-02-11 21:40:10 +0000

    Upload New File

---
+++ AmazonianBreweries/GlobalVariables.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#----------------------------------------------------------------------------
+# Created By  : Nick Santucci @nictooch
+# Created Date: February 14 2022 
+# version ='1.0.0'
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+from dataclasses import dataclass
+
+UtilizationList = ['No Orders','Starved Supply','Running (Slow)','Unknown','EStop','Sticking Valve','Pump Overload','Faulty Wiring','Tripped Breaker','Planned Maintenance','Unplanned Maintenance']
+
+UtilizationStateList = ['Demand','Downtime','Maintenance'] 
+
+ScanRates = [.97,.98,.99,.100]
+
+@dataclass
+class NewStatusEnum:
+    Allocated: str = "Allocated"
+    Filling: str = "Filling"
+    Idle: str = "Idle"
+    RampingUp: str = "Ramping Up"
+    RampingDown: str = "Ramping Down"
+    RampingUp1: str = "RampingUp1"
+    RampingUp2: str = "RampingUp2"
+    Holding: str = "Holding"
+    Holding1: str = "Holding1"
+    Holding2: str = "Holding2"
+    Draining: str = "Draining"
+    WaitingForOperator: str = "WaitingForOperator " 
+
+@dataclass
+class NewStateEnum:
+    Done: str = "Done"
+    Ready: str = "Ready"
+    Running: str = "Running"
+    Paused: str = "Paused"
+    Aborted: str = "Aborted"
